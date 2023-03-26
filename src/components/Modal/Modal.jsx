e227c3f55faf5cfbbc292dae972088c847fa8136
Totalinfonet <totalinfonet@gmail.com>
2023-03-26 13:49:33 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,50 +1,52 @@
-import React, { Component } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import Loader from '../Loader/Loader';
 
-class Modal extends Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.props.handleModalKeyDown);
-  }
+const Modal = ({
+  showModal,
+  selectedImage,
+  selectedImageLoaded,
+  handleModalClose,
+  handleImageLoad,
+  handleModalKeyDown,
+}) => {
+  const memoizedHandleModalKeyDown = useCallback(
+    event => {
+      handleModalKeyDown(event);
+    },
+    [handleModalKeyDown]
+  );
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.props.handleModalKeyDown);
-  }
+  useEffect(() => {
+    document.addEventListener('keydown', memoizedHandleModalKeyDown);
+    return () => {
+      document.removeEventListener('keydown', memoizedHandleModalKeyDown);
+    };
+  }, [memoizedHandleModalKeyDown]);
 
-  render() {
-    const {
-      showModal,
-      selectedImage,
-      selectedImageLoaded,
-      handleModalClose,
-      handleImageLoad,
-      handleModalKeyDown,
-    } = this.props;
-
-    return (
-      <>
-        {showModal && (
-          <div
-            className="overlay"
-            onClick={handleModalClose}
-            onKeyDown={handleModalKeyDown}
-            tabIndex="0"
-          >
-            <div className="modal">
-              {!selectedImageLoaded && <Loader />}
-              <img
-                src={selectedImage.largeImageURL}
-                alt=""
-                onLoad={handleImageLoad}
-                style={{ display: selectedImageLoaded ? 'block' : 'none' }}
-              />
-            </div>
+  return (
+    <>
+      {showModal && (
+        <div
+          className="overlay"
+          onClick={handleModalClose}
+          onKeyDown={memoizedHandleModalKeyDown}
+          tabIndex="0"
+        >
+          <div className="modal">
+            {!selectedImageLoaded && <Loader />}
+            <img
+              src={selectedImage.largeImageURL}
+              alt=""
+              onLoad={handleImageLoad}
+              style={{ display: selectedImageLoaded ? 'block' : 'none' }}
+            />
           </div>
-        )}
-      </>
-    );
-  }
-}
+        </div>
+      )}
+    </>
+  );
+};
 
 Modal.propTypes = {
   showModal: PropTypes.bool.isRequired,
@@ -56,3 +58,58 @@ Modal.propTypes = {
 };
 
 export default Modal;
+
+// import React, { useEffect } from 'react';
+// import PropTypes from 'prop-types';
+// import Loader from '../Loader/Loader';
+
+// const Modal = ({
+//   showModal,
+//   selectedImage,
+//   selectedImageLoaded,
+//   handleModalClose,
+//   handleImageLoad,
+//   handleModalKeyDown,
+// }) => {
+//   useEffect(() => {
+//     document.addEventListener('keydown', handleModalKeyDown);
+//     console.log('keydown');
+//     return () => {
+//       document.removeEventListener('keydown', handleModalKeyDown);
+//     };
+//   }, [handleModalKeyDown]);
+
+//   return (
+//     <>
+//       {showModal && (
+//         <div
+//           className="overlay"
+//           onClick={handleModalClose}
+//           onKeyDown={handleModalKeyDown}
+//           tabIndex="0"
+//         >
+//           <div className="modal">
+//             {!selectedImageLoaded && <Loader />}
+//             <img
+//               src={selectedImage.largeImageURL}
+//               alt=""
+//               onLoad={handleImageLoad}
+//               style={{ display: selectedImageLoaded ? 'block' : 'none' }}
+//             />
+//           </div>
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+// Modal.propTypes = {
+//   showModal: PropTypes.bool.isRequired,
+//   selectedImage: PropTypes.object.isRequired,
+//   selectedImageLoaded: PropTypes.bool.isRequired,
+//   handleModalClose: PropTypes.func.isRequired,
+//   handleModalKeyDown: PropTypes.func.isRequired,
+//   handleImageLoad: PropTypes.func.isRequired,
+// };
+
+// export default Modal;
