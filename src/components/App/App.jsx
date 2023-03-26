8e3b7f75cae175e5275b4a1cc69bd153f2d54298
Totalinfonet <totalinfonet@gmail.com>
2023-03-26 14:39:00 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Searchbar from '../Searchbar/Searchbar';
 import Notiflix from 'notiflix';
+import Searchbar from '../Searchbar/Searchbar';
 import Loader from '../Loader/Loader';
 import Button from '../Button/Button';
 import Modal from '../Modal/Modal';
@@ -10,140 +10,121 @@ import ImageGallery from '../ImageGallery/ImageGallery';
 const API_KEY = '33365759-bdd854990cd5a8ba018a7d8b1';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export class App extends Component {
-  state = {
-    images: [],
-    error: null,
-    query: '',
-    page: 1,
-    loading: false,
-    showModal: false,
-    selectedImage: null,
-    selectedImageLoaded: false,
-  };
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(null);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedImageLoaded, setSelectedImageLoaded] = useState(false);
 
-  handleSubmit = async query => {
+  const handleSubmit = async query => {
     if (query === '') {
       Notiflix.Notify.info('Enter your search query');
       return;
     } else {
-      this.clearGallery();
-      this.setState({ query, page: 1, loading: true });
+      clearGallery();
+      setQuery(query);
+      setPage(1);
+      setLoading(true);
     }
   };
 
-  componentDidUpdate(_, prevState) {
-    const { query, page } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
-      this.setState({ loading: true });
-
-      this.fetchImages();
+  useEffect(() => {
+    if (query === '') {
+      return;
     }
-  }
 
-  clearGallery = () => {
-    this.setState({ images: [] });
-  };
+    const fetchImages = async () => {
+      setLoading(true);
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-      loading: true,
-    }));
-  };
+      try {
+        const params = `?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
 
-  fetchImages = async () => {
-    const { query, page } = this.state;
-    this.setState({ loading: true });
-
-    try {
-      const params = `?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
-
-      const response = await axios.get(BASE_URL + params);
-      if (response.data.hits?.length > 0) {
-        this.setState(prevState => ({
-          images:
+        const response = await axios.get(BASE_URL + params);
+        if (response.data.hits?.length > 0) {
+          setImages(prevImages =>
             page === 1
               ? response.data.hits
-              : [...prevState.images, ...response.data.hits],
-          error: null,
-        }));
-      } else {
-        this.setState({ images: [], error: null });
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
+              : [...prevImages, ...response.data.hits]
+          );
+          setError(null);
+        } else {
+          setImages([]);
+          setError(null);
+          Notiflix.Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        }
+      } catch (error) {
+        Notiflix.Notify.failure('Failed to fetch images. Please try again.');
+        console.error(error);
+        setError(error);
+      } finally {
+        setLoading(false);
       }
-    } catch (error) {
-      Notiflix.Notify.failure('Failed to fetch images. Please try again.');
-      console.error(error);
-      this.setState({ error });
-    } finally {
-      this.setState({ loading: false });
-    }
+    };
+
+    fetchImages();
+  }, [query, page]);
+
+  const clearGallery = () => {
+    setImages([]);
   };
 
-  handleImageClick = image => {
-    this.setState({
-      showModal: true,
-      selectedImage: image,
-      selectedImageLoaded: false,
-    });
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
+    setLoading(true);
   };
 
-  handleModalClose = () => {
-    this.setState({ showModal: false });
+  const handleImageClick = image => {
+    setShowModal(true);
+    setSelectedImage(image);
+    setSelectedImageLoaded(false);
   };
 
-  handleModalKeyDown = event => {
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  const handleModalKeyDown = event => {
     if (event.key === 'Escape' || event.keyCode === 27) {
-      this.handleModalClose();
+      handleModalClose();
     }
   };
 
-  handleImageLoad = () => {
-    this.setState({ selectedImageLoaded: true });
+  const handleImageLoad = () => {
+    setSelectedImageLoaded(true);
   };
 
-  render() {
-    const {
-      images,
-      error,
-      showModal,
-      selectedImage,
-      selectedImageLoaded,
-      loading,
-    } = this.state;
+  return (
+    <div className="app">
+      <Searchbar handleSubmit={handleSubmit} />
 
-    return (
-      <div className="app">
-        <Searchbar handleSubmit={this.handleSubmit} />
+      {loading && <Loader />}
 
-        {loading && <Loader />}
+      {error && <div>Something went wrong: {error.message}</div>}
 
-        {error && <div>Something went wrong: {error.message}</div>}
+      {images.length > 0 && (
+        <ImageGallery images={images} handleImageClick={handleImageClick} />
+      )}
 
-        {images.length > 0 && (
-          <ImageGallery
-            images={images}
-            handleImageClick={this.handleImageClick}
-          />
-        )}
-
-        {!loading && images.length > 0 && images.length % 12 === 0 && (
-          <Button handleClick={this.handleLoadMore} text={'Load more'} />
-        )}
-        {showModal && (
-          <Modal
-            showModal={showModal}
-            selectedImage={selectedImage}
-            selectedImageLoaded={selectedImageLoaded}
-            handleModalClose={this.handleModalClose}
-            handleModalKeyDown={this.handleModalKeyDown}
-            handleImageLoad={this.handleImageLoad}
-          />
-        )}
-      </div>
-    );
-  }
-}
+      {!loading && images.length > 0 && images.length % 12 === 0 && (
+        <Button handleClick={handleLoadMore} text={'Load more'} />
+      )}
+      {showModal && (
+        <Modal
+          showModal={showModal}
+          selectedImage={selectedImage}
+          selectedImageLoaded={selectedImageLoaded}
+          handleModalClose={handleModalClose}
+          handleModalKeyDown={handleModalKeyDown}
+          handleImageLoad={handleImageLoad}
+        />
+      )}
+    </div>
+  );
+};
+export default App;
